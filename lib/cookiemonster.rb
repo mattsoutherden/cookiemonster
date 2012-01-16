46254352c9263c00b9d8f25a2be2093fbbe3be25
Matt Southerden <matt@localbubble.com>
2012-01-16 14:46:06 +0000

    Pull in initial WIP.

--- lib/cookiemonster.rb
+++ lib/cookiemonster.rb
@@ -1,5 +1,61 @@
 require "cookiemonster/version"
+require 'time'
 
 module CookieMonster
-  # Your code goes here...
+
+  def self.parse(set_cookie_string)
+    hash = set_cookie_string.split(/; ?/).inject({}) do |result, field|
+      field_name, field_value = field.split('=', 2)
+      result[field_name] = field_value
+      result
+    end
+
+    name, value = hash.shift
+
+    hash[:name] = name
+    hash[:value] = value
+
+    hash = hash.inject({}) do |options, (key, value)|
+      options[(key.downcase.to_sym rescue key) || key] = value
+      options
+    end
+
+    Cookie.new hash
+  end
+
+  class Cookie
+
+    attr_reader :name
+    attr_reader :value
+    attr_reader :path
+    attr_reader :domain
+    attr_reader :expires
+    attr_reader :http_only
+    attr_reader :secure
+    attr_reader :session
+    
+    alias :http_only? :http_only 
+    alias :secure? :secure 
+    alias :session? :session 
+
+    def initialize(args)
+      @name = args[:name]
+      @value = args[:value]
+      @path = args[:path]
+      @domain = args[:domain]
+      @expires = args[:expires] ? parse_expiry(args[:expires]) : nil
+      @http_only = args.has_key?(:httponly) ? true : false
+      @secure = args.has_key?(:secure) ? true : false
+      @session = expires.nil?
+    end
+
+    protected
+
+    def parse_expiry(str)
+      return Time.parse(str)
+    rescue
+      nil
+    end
+  end
+
 end
---
+++ spec/lib/raw_parse_spec.rb
@@ -0,0 +1,71 @@
+require "spec_helper"
+
+describe 'Parsing Set-Cookie' do
+
+  describe 'basic parsing' do
+
+    def parse(set_cookie)
+      CookieMonster.parse(set_cookie)
+    end
+
+    it "extracts cookie name" do
+      cookie = parse('foo=bar')
+      cookie.name.must_equal 'foo'
+
+      cookie = parse('baz=quux')
+      cookie.name.must_equal 'baz'
+    end
+
+    it "extracts cookie value" do
+      cookie = parse('foo=bar')
+      cookie.value.must_equal 'bar'
+
+      cookie = parse('baz=quux')
+      cookie.value.must_equal 'quux'
+    end
+
+    it "extracts path" do
+      cookie = parse('foo=bar; path=/')
+      cookie.path.must_equal '/'
+
+      cookie = parse('foo=bar; Path=/foo')
+      cookie.path.must_equal '/foo'
+    end
+
+    it "extracts expiry time" do
+      cookie = parse('foo=bar; expires=Thu, 19-Dec-2013 17:24:17 GMT')
+      cookie.expires.must_equal Time.utc(2013, 12, 19, 17, 24, 17)
+    end
+
+    it "sets httponly" do
+      cookie = parse('foo=bar; httponly')
+      assert cookie.http_only?
+    end
+
+    it "sets secure" do
+      cookie = parse('foo=bar; secure')
+      assert cookie.secure?
+    end
+
+    it "sets domain" do
+      cookie = parse('foo=bar; domain=foo.com')
+      cookie.domain.must_equal 'foo.com'
+    end
+
+    it "creates a session cookie when no expires is set" do
+      cookie = parse('foo=bar')
+      assert cookie.session?
+    end
+
+    it "creates a session cookie when expires parsing fails" do
+      cookie = parse('foo=bar; expires=Not a date format GMT')
+      assert cookie.session?
+    end
+
+  end
+  
+  describe 'multiple cookies' do
+    
+  end
+
+end
